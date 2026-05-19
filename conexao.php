139c3335396b73c92b625af04880ed517b4e48c3
gabrielgs-dot <gabriel.gs@sempreceub.com>
2026-05-19 19:11:45 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,15 +1,14 @@
+
 <?php
+$host = "dpg-d85qee3tqb8s73fe6lfg-a.ohio-postgres.render.com";
+$db = "sistema_os_p356";
+$user = "admin";
+$pass = "JAyzptEp830EHSGyCoKIhMBwOih7LKOQ";
 
-$conn = pg_connect("
-    host=dpg-d85qee3tqb8s73fe6lfg-a.ohio-postgres.render.com
-    port=5432
-    dbname=sistema_os_p356
-    user=admin
-    password=JAyzptEp830EHSGyCoKIhMBwOih7LKOQ
-    sslmode=require
-");
-
-if (!$conn) {
-    die("Erro na conexão com PostgreSQL");
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro de conexão: " . $e->getMessage());
 }
 ?>
