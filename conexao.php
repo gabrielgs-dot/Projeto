14dccb56bb1c0922e4c3ab788f1e1ce68471c712
gabrielgs-dot <gabriel.gs@sempreceub.com>
2026-05-19 19:42:02 -0300

    conexao.php

--- conexao.php
+++ conexao.php
@@ -1,15 +1,16 @@
- 
 <?php
 
-$host = "dpg-d85qee3tqb8s73fe6lfg-a.ohio-postgres.render.com";
-$user = "admin";
-$pass = "JAyzptEp830EHSGyCoKIhMBwOih7LKOQ";
-$db   = "sistema_os_p356";
+$host = "localhost";
+$usuario = "root";
+$senha = "paris1308";
+$banco = "sistema_os";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $usuario, $senha, $banco);
 
 if ($conn->connect_error) {
-    die("Erro de conexão: " . $conn->connect_error);
+    die("Erro na conexão: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8");
 
 ?>
