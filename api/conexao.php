0c0e3c7e3c129e944dd333938fc60cdc9d1aa658
gabrielgs-dot <gabriel.gs@sempreceub.com>
2026-05-18 21:28:49 -0300

    Add files via upload

---
+++ api/conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+$conn = pg_connect("
+    host=dpg-d85qee3tqb8s73fe6lfg-a.ohio-postgres.render.com
+    port=5432
+    dbname=sistema_os_p356
+    user=admin
+    password=JAyzptEp830EHSGyCoKIhMBwOih7LKOQ
+    sslmode=require
+");
+
+if (!$conn) {
+    die("Erro na conexão com PostgreSQL");
+}
+?>
